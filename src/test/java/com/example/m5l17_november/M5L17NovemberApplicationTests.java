3d1c0a68a47b934e03ac610fa83c493bb5111436
mentor <kenku@mail.ru>
2024-02-20 23:19:15 +0300

    M5L17_November

---
+++ src/test/java/com/example/m5l17_november/M5L17NovemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m5l17_november;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5L17NovemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
